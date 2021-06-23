ac3399cd269e619e21f193e7682ba416b4fbba95
Jinmun Park <57411806+Jinmun-Park@users.noreply.github.com>
2021-06-23 19:46:37 +0800

    Update classes.py

--- classes.py
+++ classes.py
@@ -127,3 +127,67 @@ if __name__ == '__main__':
     angle = math.acos(x.dot(y) / (x.absolute() * y.absolute()))
 
     print("%.2f" % math.degrees(angle))
+################################################################################################################################################################
+'''
+Debugging
+Default Arguments
+
+Sample Input
+3
+odd 2
+even 3
+odd 5
+
+Sample Output
+1
+3
+0
+2
+4
+1
+3
+5
+7
+9
+
+Explanation
+
+'''
+
+class EvenStream(object):
+    def __init__(self):
+        self.current = 0
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+class OddStream(object):
+    def __init__(self):
+        self.current = 1
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+def print_from_stream(n, stream=None): 
+#def print_from_stream(n, stream=EvenStream()): This does make EvenStream default
+    if stream is None: # you have to se this to make the function works
+        stream = EvenStream()
+        
+    for _ in range(n):
+        print(stream.get_next())
+        
+
+
+queries = int(input())
+for _ in range(queries):
+    stream_name, n = input().split()
+    n = int(n)
+    if stream_name == "even":
+        print_from_stream(n)
+    else:
+        print_from_stream(n, OddStream())
+
