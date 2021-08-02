a2b7a2c61475568ea277324dbd16523ce7997119
Jinmun Park <57411806+Jinmun-Park@users.noreply.github.com>
2021-08-02 20:22:02 +0800

    Update JOIN.sql

--- sql/JOIN.sql
+++ sql/JOIN.sql
@@ -70,3 +70,40 @@ WHERE P.is_evil = 0 AND W.coins_needed = '''Looking for minimun gold need '''
         INNER JOIN WANDS_PROPERTY AS P1 ON W1.code = P1.code
      WHERE W1.power = W.power AND P1.age = P.age) '''Question is poorly stated that it just needs combination of power&age to find the minum gold required'''
 ORDER BY W.power DESC, P.age DESC
+
+''' ****************************************************************************************************** DIFFICULT
+NAME : Challenges
+TYPE : Basic Join
+
+Write a query to print the hacker_id, name, and the total number of challenges created by each student. 
+Sort your results by the total number of challenges in descending order. 
+If more than one student created the *same number of challenges, then sort the result by hacker_id. 
+If more than one student created the *same number of challenges and the count is less than *the maximum number of challenges created, 
+then exclude those students from the result.
+'''
+SELECT h.hacker_id, h.name, COUNT(c.challenge_id) AS challenge_counter
+FROM hackers h
+JOIN challenges c
+    ON h.hacker_id = c.hacker_id
+GROUP BY h.hacker_id, h.name
+
+HAVING challenge_counter IN ( '''Condition 1 : unique count of channge_id'''
+    SELECT aux_table.counter
+    FROM(
+        SELECT hacker_id, COUNT(challenge_id) AS counter 
+        FROM challenges
+        GROUP BY hacker_id '''Group By 1'''
+    ) AS aux_table
+    GROUP BY aux_table.counter '''Group By 2, reason for 2 groupby is because we have to sort by hacker_id '''
+    HAVING COUNT(aux_table.counter) = 1
+)
+OR
+challenge_counter =(
+    SELECT MAX(aux_table.counter)
+    FROM(
+        SELECT hacker_id, COUNT(challenge_id) AS counter
+        FROM challenges
+        GROUP BY hacker_id
+    ) AS aux_table)
+ORDER BY challenge_counter DESC, h.hacker_id ASC;
+
