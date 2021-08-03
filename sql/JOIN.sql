7bd02abb65b7d9f476f7ee212a944e860a135d9b
Jinmun Park <57411806+Jinmun-Park@users.noreply.github.com>
2021-08-04 01:07:09 +0800

    Update JOIN.sql

--- sql/JOIN.sql
+++ sql/JOIN.sql
@@ -76,11 +76,17 @@ If more than one wand has same power, sort the result in order of descending age
 SELECT W.id, P.age, W.coins_needed, W.power
 FROM WANDS AS W
     INNER JOIN WANDS_PROPERTY AS P ON W.code = P.code
-WHERE P.is_evil = 0 AND W.coins_needed = '''Looking for minimun gold need '''
-    (SELECT MIN(coins_needed)
-     FROM WANDS AS W1
-        INNER JOIN WANDS_PROPERTY AS P1 ON W1.code = P1.code
-     WHERE W1.power = W.power AND P1.age = P.age) '''Question is poorly stated that it just needs combination of power&age to find the minum gold required'''
+WHERE P.is_evil = 0 AND (W.coins_needed, P.age, W.power) IN  
+    (SELECT 
+        MIN(W1.coins_needed),
+        P1.age, 
+        W1.power
+    FROM WANDS AS W1 INNER JOIN 
+        WANDS_PROPERTY AS P1 ON 
+        W1.code = P1.code
+    GROUP BY 
+        P1.age, 
+        W1.power)
 ORDER BY W.power DESC, P.age DESC
 
 ''' ****************************************************************************************************** DIFFICULT
