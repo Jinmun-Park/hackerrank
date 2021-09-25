69e174d1211f4be5227dee0c1bfb43344b0be3a6
Jinmun Park <57411806+Jinmun-Park@users.noreply.github.com>
2021-09-25 15:36:40 +0800

    Update CASE.sql

--- sql/CASE.sql
+++ sql/CASE.sql
@@ -11,16 +11,17 @@ Scalene: Its a triangle with  sides of differing lengths.
 Not A Triangle: The given values of A, B, and C dont form a triangle.
 '''
 
-SELECT CASE             
-    WHEN A + B > C AND B + C > A AND A + C > B THEN
-        CASE 
-            WHEN A = B AND B = C THEN 'Equilateral'
-            WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+SELECT CASE
+    WHEN A+B>C AND B+C>A AND A+C>B THEN
+        CASE
+            WHEN  A=B AND B=C AND A=C THEN 'Equilateral'
+            WHEN A=B OR B=C OR A=C THEN 'Isosceles'
             ELSE 'Scalene'
         END
     ELSE 'Not A Triangle'
     END
-FROM TRIANGLES;
+FROM
+    TRIANGLES 
 
 ''' ******************************************************************************************************
 NAME : The PADS
