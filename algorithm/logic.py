bb8b6ac8ae0da129ff17efc741aca70dbed1c6be
Jinmun Park <57411806+Jinmun-Park@users.noreply.github.com>
2022-02-18 16:53:20 +0900

    Update logic.py

--- algorithm/logic.py
+++ algorithm/logic.py
@@ -63,31 +63,7 @@ NO
 '''
 
 def kangaroo(x1, v1, x2, v2):
-    # Write your code here
-    if v1 == v2 and x1 != x2: # if same speed, they will never meet
-        return('NO')
-    if x1 < x2 and v1 < v2:
-        return('NO')
-    if x1 > x2 and v1 > v2:
-        return('NO')
-    if x1 < x2 and v1 > v2:
-        while x1 != x2: '''IMPORTANT'''
-            x1 = x1 + v1
-            x2 = x2 + v2
-            if x1 == x2:
-                return ('YES')
-            if x1 > x2:
-                return ('NO')
-                break '''IMPORTANT'''
-    if x1 > x2 and v1 < v2:
-        while x1 != x2:
-            x1 = x1 + v1
-            x2 = x2 + v2
-            if x1 == x2:
-                return ('YES')
-            if x1 < x2:
-                return ('NO')
-                break
+    return 'YES' if (v1 > v2) and (x2 - x1) % (v2 - v1) == 0 else 'NO'
                 
 ########################################################################################################################
 '''
